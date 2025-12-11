1c62258a440fc5decf26e49e675071605e09f972
Muhammad Waleed Tariq <81246644+Weed-eng@users.noreply.github.com>
2025-12-11 15:32:02 +0000

    Delete turtlebot directory (unused)

--- turtlebot/controllers/motion_planner/__init__.py
+++
@@ -1,7 +0,0 @@
-# motion planner module - handles robot movement and path planning
-# modular structure: config, planner, odometry
-
-from motion_planner.planner import MotionPlannerController
-
-__all__ = ['MotionPlannerController']
-__version__ = '0.1.0'
--- turtlebot/controllers/turtlebot3_ostacle_avoidance/build/release/turtlebot3_ostacle_avoidance.d
+++
@@ -1,8 +0,0 @@
-build/release/turtlebot3_ostacle_avoidance.o: \
- turtlebot3_ostacle_avoidance.c \
- C:\Program\ Files\Webots/include/controller/c/webots/lidar.h \
- C:\Program\ Files\Webots/include/controller/c/webots/lidar_point.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h
